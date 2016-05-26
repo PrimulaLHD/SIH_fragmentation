389b4ad31111ab1a9174973856263a3735e0ff9a
plthompson <patrickthompson9@gmail.com>
2016-05-26 13:05:38 -0600

    Updated code to use circuitscape distances for dispersal

--- Realistic networks dynamics.R
+++ Realistic networks dynamics.R
@@ -7,7 +7,7 @@ library(ggExtra)
 library(doParallel)
 library(foreach)
 
-SIH_function<-function(dispV=NA,species=15,numCom=100){
+SIH_function<-function(dispV=NA,species=10,numCom=100){
   #Constants####
   #N<- matrix(10,ncol=species,nrow=numCom) # Community x Species abundance matrix
   rInput<-150 #resource input
@@ -48,13 +48,31 @@ SIH_function<-function(dispV=NA,species=15,numCom=100){
   
   plot.igraph(graph,layout=as.matrix(landscape), vertex.color=heat.colors(100)[1+(envt.v*99)], vertex.size=5000,vertex.label=NA, rescale=F, ylim=c(0,1000),xlim=c(0,1000))
   
+  CS_exe<-'C:/"Program Files"/Circuitscape/cs_run.exe'
+  
   #dispersal conditions####
-  d<-shortest.paths(graph, mode="all", weights=NULL, algorithm="automatic")
-  d_exp<-exp(-0.002*d) - diag(nrow(d))  #dispersal kernel function of the d matrix
+  graph_circuit<-data.frame(get.edgelist(graph), E(graph)$weight)
+  write.table(graph_circuit, paste("./Circuits/network_graph",r,".txt", sep="_"), row.names = FALSE, col.names = FALSE, quote = FALSE)
+  write.table(V(graph)$name, paste("./Circuits/network_focal_nodes",r,".txt", sep="_"), row.names = FALSE, col.names = FALSE, quote = FALSE)
+  CS_ini <- c("[circuitscape options]",            
+              "data_type = network",
+              "scenario = pairwise",
+              paste(c("point_file =",
+                      "habitat_file =",
+                      "output_file ="),
+                    c(paste("./Circuits/network_focal_nodes",r,".txt", sep="_"),
+                      paste("./Circuits/network_graph",r,".txt", sep="_"),
+                      paste("./Circuits/CS",r,".out",sep="_"))))
+  
+  writeLines(CS_ini, paste("./Circuits/my_ini",r,".ini", sep="_"))
+  CS_run <- paste(CS_exe, paste("./Circuits/my_ini",r, ".ini", sep="_")) # Make the cmd
+  system(CS_run)
+  d <- read.table(paste("./Circuits/CS",r,"_resistances.out",sep="_"), row.names=1,header=TRUE)
+  #d<-shortest.paths(graph_run, mode="all", weights=NULL, algorithm="automatic")
+  #d_exp<-exp(-0.002*d) - diag(nrow(d))  #dispersal kernel function of the d matrix
+  d_exp<-exp(-0.05*d) - diag(nrow(d))
   dispersal_matrix <- apply(d_exp, 1, function(x) x/sum(x)) #divides the d_exp matrix by the column sums to make it a conservative dispersal matrix
-  
-  #dispersal_matrix <- apply(connections, 1, function(x) x/sum(x)) #divides the d_exp matrix by the column sums to make it a conservative dispersal matrix
-  
+  rownames(dispersal_matrix)<-1:numCom
   
   calc.immigration <- function(N,a,dispersal_matrix) dispersal_matrix%*%N*rep(a,each=numCom)
   
@@ -66,11 +84,12 @@ SIH_function<-function(dispV=NA,species=15,numCom=100){
     
     print(i)
     dispersal<-dispV[i]
-    Prod<-matrix(NA,species*numCom,40000)
-    Abund<-Prod
+    sampleV<-seq(100000,Tmax,by=100)
+    Abund<-matrix(NA,species*numCom,length(sampleV))
     
-    Meta_dyn<-data.frame(Species_sorting=rep(NA,40000),Mass_effects=NA,Base_growth=NA)
-    Species_data<-array(NA,dim=c(40000,species,2),dimnames = list(1:40000,1:species,c("Abundance","Occupancy")))
+    
+    Meta_dyn<-data.frame(Species_sorting=rep(NA,length(sampleV)),Mass_effects=NA,Base_growth=NA)
+    Species_data<-array(NA,dim=c(length(sampleV),species,2),dimnames = list(sampleV,1:species,c("Abundance","Occupancy")))
     
     for(TS in 1:Tmax){
       envt.v<-0.5*eAMP*(sin((2*pi/ePeriod)*TS+1+(landscape$y)*2*pi/1000)+1)
@@ -84,9 +103,9 @@ SIH_function<-function(dispV=NA,species=15,numCom=100){
       Rt <- DT*rInput+R*(1-DT*(rLoss + rowSums(consume*N))) #resource step   
       Rt0 <- DT*rInput+R0*(1-DT*(rLoss + rowSums(consume*N0))) #resource step  
       
-      if(TS>=100000){
-        Prod[,(TS-100000)] <- c(t(eff*consume*R*N))
-        Abund[,(TS-100000)] <- c(t(N))
+      if(sum(TS==sampleV)==1){
+        sampleValue<-which(sampleV==TS)
+        Abund[,sampleValue] <- c(t(N))
         
         fitness<-((N*(1+DT*(eff*R*consume - dispersal - mort)))-N)*(Nt>Ext)
         fitness_w_disp<-((N*(1+DT*(eff*R*consume - dispersal - mort)) + DT*Immigrants)-N)*(Nt>Ext)
@@ -104,20 +123,20 @@ SIH_function<-function(dispV=NA,species=15,numCom=100){
         SS<-(SS_prod/home_prod)*home_prod_prop
         SS[is.nan(SS)]<-0
         if(total_prod==0){SS<-NA}
-        Meta_dyn$Species_sorting[(TS-100000)]<-SS
+        Meta_dyn$Species_sorting[sampleValue]<-SS
         
         ME<-(disp_prod_ME)/total_prod
         ME[is.nan(ME)]<-0
         if(total_prod==0){ME<-NA}
-        Meta_dyn$Mass_effects[(TS-100000)]<-ME
+        Meta_dyn$Mass_effects[sampleValue]<-ME
         
         BP<-home_prod_prop*(1-(SS_prod/home_prod))
         BP[is.nan(BP)]<-0
         if(total_prod==0){BP<-NA}
-        Meta_dyn$Base_growth[(TS-100000)]<-BP
+        Meta_dyn$Base_growth[sampleValue]<-BP
         
-        Species_data[(TS-100000),,1]<-colSums(N)
-        Species_data[(TS-100000),,2]<-colSums(N>0)
+        Species_data[sampleValue,,1]<-colSums(N)
+        Species_data[sampleValue,,2]<-colSums(N>0)
       }
       N <- Nt * (Nt>Ext) # set to 0 if below extinction threshold
       R <- Rt
@@ -126,24 +145,20 @@ SIH_function<-function(dispV=NA,species=15,numCom=100){
       R0 <- Rt0
     } 
     
-    Prod<-array(t(Prod),dim=c(40000,species,numCom))
-    Prod<-Prod[seq(1,40000,100),,]
+    Abund<-array(t(Abund),dim=c(length(sampleV),species,numCom))
     
-    Abund<-array(t(Abund),dim=c(40000,species,numCom))
-    Abund<-Abund[seq(1,40000,100),,]
-    
-  hold_data<-data.frame(L_SR=mean(apply(Abund>0,3,rowSums)),
-               R_SR=mean(rowSums(apply(Abund,2,rowSums)>0)), 
-               L_Biomass= mean(apply(Abund,3,rowSums)),
-               Base_growth=mean(Meta_dyn$Base_growth),
-               Species_sorting=mean(Meta_dyn$Species_sorting),
-               Mass_effects=mean(Meta_dyn$Mass_effects),
-               Dispersal=dispersal)
-  if(i==1){
-    SIH_data<-hold_data
-  } else {
-    SIH_data<-rbind(SIH_data,hold_data)
-  }
+    hold_data<-data.frame(L_SR=mean(apply(Abund>0,3,rowSums)),
+                          R_SR=mean(rowSums(apply(Abund,2,rowSums)>0)), 
+                          L_Biomass= mean(apply(Abund,3,rowSums)),
+                          Base_growth=mean(Meta_dyn$Base_growth,na.rm=T),
+                          Species_sorting=mean(Meta_dyn$Species_sorting,na.rm=T),
+                          Mass_effects=mean(Meta_dyn$Mass_effects,na.rm=T),
+                          Dispersal=dispersal)
+    if(i==1){
+      SIH_data<-hold_data
+    } else {
+      SIH_data<-rbind(SIH_data,hold_data)
+    }
   }
   return(SIH_data)
 }
@@ -152,15 +167,16 @@ vect<-c(0.0001,0.00015,0.00025,0.0005,0.00075)
 dispV<-c(vect,vect*10,vect*100,vect*1000,1)
 dispV<-dispV[-c(17:20)]
 
-reps<-100
-Meta_dyn.df<-data.frame(Species_sorting=NA,Mass_effects=NA,Base_growth=NA,Dispersal=dispV,Rep=rep(1:reps,each=length(dispV)))
+
+reps<-16
 
 #make parallel####
-cl<-makeCluster(2)
+cl<-makeCluster(detectCores())
 registerDoParallel(cl)
 getDoParWorkers()
 
 Sim_data_parallel<-foreach(r = 1:reps,.packages=c("igraph","dplyr","tidyr")) %dopar% SIH_function(species = 10,numCom = 100, dispV=dispV)
+stopCluster(cl)
 
 Sim_data<-do.call("rbind",Sim_data_parallel)
 
@@ -170,18 +186,15 @@ SIH_means<-Sim_data_long%>%
   group_by(Dispersal,Response)%>%
   summarise_each(funs(Mean=mean(.,na.rm=T),Lower=quantile(.,probs = 0.25,na.rm=T),Upper=quantile(.,probs=0.75,na.rm=T)))
 
-stopCluster(cl)
 
-save(SIH_means,file="Meta_dynamics.RData")
+Meta.dyn.df<-filter(SIH_means,Response=="Base_growth" |
+                      Response == "Species_sorting" |
+                      Response == "Mass_effects")
 
-Meta_dyn<-filter(SIH_means,Response == "Base_growth" | 
-         Response == "Species_sorting" |
-         Response == "Mass_effects")
-
-Meta_dyn$Response<-factor(Meta_dyn$Response,levels =c("Base_growth","Species_sorting", "Mass_effects"),ordered = T)
+Meta.dyn.df$Response<-factor(Meta.dyn.df$Response,levels =c("Base_growth","Species_sorting", "Mass_effects"),ordered = T)
 
 
-ggplot(Meta_dyn,aes(x=Dispersal,y=Mean,color=Response, fill=Response))+
+ggplot(Meta.dyn.df,aes(x=Dispersal,y=Mean,color=Response, fill=Response))+
   geom_ribbon(aes(ymin = Lower, ymax = Upper),alpha=0.3,color=NA)+
   geom_line(size=1.5)+
   theme_bw(base_size = 15)+
@@ -193,4 +206,6 @@ ggplot(Meta_dyn,aes(x=Dispersal,y=Mean,color=Response, fill=Response))+
   ylab("Propotion of biomass production")+
   theme(legend.justification=c(1,0),legend.position=c(1,0.5))+
   geom_vline(xintercept = c(0.0005,0.005,0.015), linetype=2)
-ggsave("2. Metacommunity dynamics.pdf",width = 8,height = 6)
+
+
+save(SIH_means,file="Meta_dynamics.RData")
